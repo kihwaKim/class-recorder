1c0131362eff4e7a032920a1fe7eb36c9891dfd4
김기화 <kwnyw1220@gmail.com>
2025-07-29 14:55:22 +0900

    q

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 if not firebase_admin._apps:
     cred = credentials.Certificate("firebase_key.json")
     initialize_app(cred, {
-        'storageBucket': 'your-bucket-name.appspot.com'
+        'storageBucket': 'class-recoder-ca0ea.firebasestorage.app'
     })
 
 db = firestore.client()
